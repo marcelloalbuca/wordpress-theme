9befa10312a27e6de7c5b715d6f8967575ed7540
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-02-13 12:19:17 -0300

    criando parte de blog

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
 
+<div class="blog">
+    <div class="wrap">
+        <h2>BLOG<h2>
+
+        <div class="box-liste-posts">
+            <div class="liste-posts">
+                <img src="<?php bloginfo('template_directory' );  ?>/assets/images/img-posts.png" alt="">
+
+                <h2> Teste Marcello <h2>
+                <p> TesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTeste </p>
+                <a href="#" class="custom-botao">Leia mais</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <?php get_footer(); ?>
