44649211166e96fe84d38727c6dc4b6818a8a93a
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-02-13 12:44:00 -0300

    estrutura blog

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
             <div class="liste-posts">
                 <img src="<?php bloginfo('template_directory' );  ?>/assets/images/img-posts.png" alt="">
 
-                <h2> Teste Marcello <h2>
-                <p> TesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTeste </p>
-                <a href="#" class="custom-botao">Leia mais</a>
+                <div class="box-content-post">
+                    <h2> Teste Marcello <h2>
+                    <p> TesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTeste </p>
+                    <a href="#" class="custom-botao">Leia mais</a>
+                </div>
+
             </div>
         </div>
     </div>
